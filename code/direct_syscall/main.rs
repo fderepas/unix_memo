821be24dc88812bc91ce75707ede7d47afa8a3da
fabrice <fabrice.derepas@polytechnique.org>
2025-03-23 14:21:21 +0100

    commented the rust version of syscalls

--- code/direct_syscall/main.rs
+++ code/direct_syscall/main.rs
@@ -1,17 +1,22 @@
 use std::arch::asm;
 
 fn main() {
-    let syscall_number : i32 = 1;                // 4 bytes
-    let file_descriptor : u64 = 1;               // 4 bytes
+    // variable representing the Linux syscall we want to call
+    let syscall_number : i32 = 1;                    // 4 bytes
+    // file descriptor on which to write
+    let file_descriptor : u64 = 1;                   // 4 bytes
+    // buffer to write
     let message = b"Hello, world!\n";
-    let message_length : usize = message.len();  // 8 bytes
+    // number of bytes to write
+    let message_length : usize = message.len();      // 8 bytes
+    // pointer to the buffer
     let message_ptr : * const u8 = message.as_ptr(); // 8 bytes
     unsafe {
         asm!(
-            "mov eax, {0:e}",
-            "mov edi, {1:e}",
-            "mov rdx, {2:r}",
-            "mov rsi, {3:r}",
+            "mov eax, {0:e}", // move argument #0 (syscall_number) in eax
+            "mov edi, {1:e}", // move argument #1 (file_descriptor) in edi
+            "mov rdx, {2:r}", // move argument #2 (message_length) in rdx
+            "mov rsi, {3:r}", // move argument #3 (message_ptr) in rsi
             "syscall",
             in(reg) syscall_number,
             in(reg) file_descriptor,
